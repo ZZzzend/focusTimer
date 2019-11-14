c86a5a92fb8c8add1142fb77d894c6599422e12a
Владислав <Vladislav@MacBook-Pro-Temp.local>
2019-11-14 13:20:58 +0800

    added timers,buttons,realm

---
+++ Focus Timer/Model/ModelTimer.swift
@@ -0,0 +1,8 @@
+//
+//  ModelTimer.swift
+//  Focus Timer
+//
+//  Created by Владислав on 12.11.2019.
+//
+
+import Foundation
---
+++ Focus Timer/Model/RealmModel.swift
@@ -0,0 +1,8 @@
+//
+//  RealmModel.swift
+//  Focus Timer
+//
+//  Created by Владислав on 12.11.2019.
+//
+
+import Foundation
